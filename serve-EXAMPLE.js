16c2b38b8e3afb5aad24781ae807138910e072b1
Ivan Becerra <becerraivan79@gmail.com>
2025-06-07 14:04:07 -0300

    API UTN - PPS

---
+++ serve-EXAMPLE.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const admin = require('firebase-admin');
+require('dotenv').config();
+
+//const serviceAccount = require(process.env.SERVICE_ACCOUNT);
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/send-push', async (req, res) => {
+  const { token, title, body } = req.body;
+
+  const message = {
+    notification: { title, body },
+    token
+  };
+
+  try {
+    const response = await admin.messaging().send(message);
+    res.json({ success: true, response });
+  } catch (error) {
+    res.status(500).json({ success: false, error });
+  }
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
