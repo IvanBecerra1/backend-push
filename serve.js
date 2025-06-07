3febb2cbfc20fda6b26aa4977a3ec2c47faa49c1
Ivan Becerra <becerraivan79@gmail.com>
2025-06-07 13:21:04 -0300

    parse JSON en ves de archivo file

--- serve.js
+++ serve.js
@@ -3,7 +3,8 @@ const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
 require('dotenv').config();
 
-const serviceAccount = require(process.env.SERVICE_ACCOUNT);
+//const serviceAccount = require(process.env.SERVICE_ACCOUNT);
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
